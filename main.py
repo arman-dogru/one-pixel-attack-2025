1f426244dc8a8ff10fc2c0d1e857cfcdc4371f78
AlexanderDeFuria <defuria.alexander@gmail.com>
2025-03-26 11:53:13 -0400

    whoops, we're downloading now

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras.datasets import cifar10
 import pickle
+import os
+from huggingface_hub import snapshot_download, login, HfApi
 
 # Custom Networks
 from networks.lenet import LeNet
@@ -17,10 +19,8 @@ from networks.wide_resnet import WideResNet
 from networks.capsnet import CapsNet
 
 # Helper functions
-from differential_evolution import differential_evolution
 import helper
 from attack import PixelAttacker
-
 
 
 if __name__ == "__main__":
@@ -33,6 +33,18 @@ if __name__ == "__main__":
         "wide_resnet": WideResNet,
         "capsnet": CapsNet,
     }
+
+    login(
+        new_session=False,  # Won’t request token if one is already saved on machine
+        write_permission=True,  # Requires a token with write permission
+        token=os.environ["HF_TOKEN_WRITE"],
+    )
+    os.makedirs("networks/models", exist_ok=True)
+    snapshot_download(
+        repo_id="Ethics2025W/base",
+        local_dir="networks/models",
+    )
+    exit(0)
 
     parser = argparse.ArgumentParser(description="Attack models on Cifar10")
     parser.add_argument(
@@ -88,7 +100,6 @@ if __name__ == "__main__":
         help="Set this switch to test the defense against the attack.",
     )
 
-
     args = parser.parse_args()
 
     # Load data and model
@@ -107,9 +118,8 @@ if __name__ == "__main__":
     ]
     models = [model_defs[m](load_weights=True) for m in args.model]
 
-
     def predict_classes(xs, img, target_class, model, minimize=True):
-        """ 
+        """
         This is the function to be minimized.
         RQ3 - TODO This function should be adjusted to minimize the change in the image
 
@@ -120,20 +130,24 @@ if __name__ == "__main__":
         minimize: bool
         """
         # Perturb the image with the given pixel(s) x and get the prediction of the model
-        imgs_perturbed = helper.perturb_image(xs, img) # Shape: (population_size, 32, 32, 3)
+        imgs_perturbed = helper.perturb_image(
+            xs, img
+        )  # Shape: (population_size, 32, 32, 3)
         predictions = model.predict(imgs_perturbed)[:, target_class]
-        
+
         # Change pixel values of perturbed image from original image
         # L1
-        diff = imgs_perturbed - img # Shape: (population_size, 32, 32, 3)
+        diff = imgs_perturbed - img  # Shape: (population_size, 32, 32, 3)
         # L2
         # Mean of the change in pixel values
         change = np.mean(np.abs(diff), axis=(1, 2, 3))
         print(change)
 
         # This function should always be minimized, so return its complement if needed
-        predictions = predictions if minimize else 1 - predictions # Confidence should go down
-        change = change if minimize else 1 - change # Change should go down
+        predictions = (
+            predictions if minimize else 1 - predictions
+        )  # Confidence should go down
+        change = change if minimize else 1 - change  # Change should go down
         score = predictions + change
         return score
 
@@ -172,13 +186,12 @@ if __name__ == "__main__":
 
     print(results_table[["model", "pixels", "image", "true", "predicted", "success"]])
 
-    for index, result in results_table[results_table['success']].iterrows():
+    for index, result in results_table[results_table["success"]].iterrows():
         # Subtract the original image to get the perturbation
-        difference = np.abs(result['attack_image'] - result['original_image'])
+        difference = np.abs(result["attack_image"] - result["original_image"])
 
         print("Mean: ", difference.mean())
         print("Max: ", difference.max())
-        
 
     print("Saving to", args.save)
     with open(args.save, "wb") as file:
